737d8cb3c454f392da15ba14cfd849f19ecbe978
iammrjude <judeabara@gmail.com>
2024-05-03 07:45:11 +0100

    responsive

--- app/page.jsx
+++ app/page.jsx
@@ -18,46 +18,32 @@ export default function Home() {
 
       {/* Body */}
       <div>
-        <div className="flex flex-col items-start bg-revolutionizing bg-cover mt-2 p-16">
-          <p className="text-white font-semibold md:text-2xl lg:text-5xl mt-48">Revolutionizing Waste Management: Building a Greener</p>
-          <p className="text-white font-semibold md:text-2xl lg:text-5xl mt-2">Future Together with GREENWAVE</p>
-          <p className="text-gray-400 md:text-base lg:text-2xl mt-5">Download App on Google Playstore or Apple Appstore</p>
-          <div className="flex flex-row space-x-8 mt-8 mb-72">
-            <Link href="#" className="bg-[#056B34] md:w-[300px] md:h-[50px] lg:w-[407px] lg:h-[70px] rounded-md text-white flex justify-center items-center text-xs md:text-base lg:text-xl font-semibold">
+        <div className="flex flex-col items-start bg-revolutionizing bg-cover mt-2 p-6 md:p-16">
+          <p className="text-white font-semibold text-xl md:text-2xl lg:text-5xl mt-40 md:mt-48 lg:mt-48">Revolutionizing Waste Management: Building a Greener</p>
+          <p className="text-white font-semibold text-xl md:text-2xl lg:text-5xl mt-2">Future Together with GREENWAVE</p>
+          <p className="text-gray-400 text-sm md:text-base lg:text-2xl mt-5">Download App on Google Playstore or Apple Appstore</p>
+          <div className="flex flex-row space-x-4 md:space-x-8 mt-8 mb-24 md:mb-36 lg:mb-72">
+            <Link href="#" className="bg-[#056B34] h-8 w-48 md:w-[300px] md:h-[50px] lg:w-[407px] lg:h-[70px] rounded-md text-white flex justify-center items-center text-xs md:text-base lg:text-xl font-semibold">
               Download on Appstore
-              {/* <Image
-                className="ml-3"
-                src="/appstore.png"
-                alt="appstore icon"
-                width={30}
-                height={30}
-              /> */}
-              <AppleIcon className="text-3xl ml-3" />
+              <AppleIcon className="text-2xl md:text-3xl ml-1 md:ml-3" />
             </Link>
-            <Link href="#" className="bg-[#056B34] md:w-[300px] md:h-[50px] lg:w-[407px] lg:h-[70px] rounded-md text-white flex justify-center items-center text-xs md:text-base lg:text-xl font-semibold">
+            <Link href="#" className="bg-[#056B34] h-8 w-48 md:w-[300px] md:h-[50px] lg:w-[407px] lg:h-[70px] rounded-md text-white flex justify-center items-center text-xs md:text-base lg:text-xl font-semibold">
               Download on Playstore
-              {/* <Image
-                className="ml-3"
-                src="/playstore.png"
-                alt="playstore icon"
-                width={35}
-                height={35}
-              /> */}
-              <PlaystoreIcon className="text-2xl ml-3" />
+              <PlaystoreIcon className="text-lg md:text-2xl ml-2 md:ml-3" />
             </Link>
           </div>
         </div>
-        <div className="my-10 px-40 text-[29px] text-black">
+        <div className="my-10 px-10 md:px-20 lg:px-40 text-sm md:text-base lg:text-[29px] text-black text-justify">
           At Greenwave, we offer educational modules on proper waste sorting, connect
           users with recycling programs and composting initiatives, and allow real-time
           waste collection tracking and reporting. Greenwave promotes responsible
           waste disposal practices, improves waste collection efficiency, and fosters a
           cleaner and healthier Port Harcourt.
         </div>
-        <div className="flex flex-col bg-welcome bg-cover text-black pt-24 pb-36">
-          <div className="mx-48 bg-white rounded-md py-24 px-10 items-start">
-            <p className="font-semibold text-3xl">Welcome to Greenwave</p>
-            <p className="mt-8 text-2xl">
+        <div className="flex flex-col bg-welcome bg-cover text-black pt-12 pb-16 md:pt-24 md:pb-36">
+          <div className="mx-12 md:mx-24 lg:mx-48 bg-white rounded-md py-6 px-5 md:py-12 md:px-5 lg:py-24 lg:px-10 items-start">
+            <p className="font-semibold text-base md:text-xl lg:text-3xl">Welcome to Greenwave</p>
+            <p className="mt-2 md:mt-4 lg:mt-8 text-sm md:text-lg lg:text-2xl text-justify">
               Maintaining a clean environment is not just a responsibility but a necessity
               for the well-being of our planet and future generations. The environment
               sustains life in all its forms, providing us with resources, clean air, and water.
@@ -68,13 +54,13 @@ export default function Home() {
             </p>
           </div>
         </div>
-        <div className="mx-16 my-20 text-black text-[27px] font-medium">
+        <div className="mx-16 my-20 text-black text-justify text-base md:text-xl lg:text-[27px] font-medium">
           Greenwave is designed to empower residents and revolutionize waste management in the
           city. We constantly strive to find new ways of tackling each of these waste streams and
           new areas of need that would benefit from Greenwave&apos;s support.
         </div>
         <div className="flex flex-col items-center mt-4 mb-20">
-          <p className="text-black font-bold text-3xl">Our Services</p>
+          <p className="text-black font-bold text-xl md:text-2xl lg:text-3xl">Our Services</p>
           <Image
             className="mt-6"
             src="/our_services.png"
@@ -83,10 +69,10 @@ export default function Home() {
             height={341}
           />
         </div>
-        <div className="flex flex-row bg-request-service bg-cover px-24 space-x-5">
-          <div className="flex flex-col mt-40">
-            <p className="text-white font-semibold text-5xl w-[640px]">Become a client and receive exclusive service offerings.</p>
-            <button className="bg-black text-white rounded-md w-64 h-16 text-xl font-medium mt-4">Request Service</button>
+        <div className="flex flex-row bg-request-service bg-cover px-6 md:px-12 lg:px-24 space-x-5">
+          <div className="flex flex-col mt-10 md:mt-20 lg:mt-40">
+            <p className="text-white font-semibold text-base md:text-2xl lg:text-5xl w-[640px]">Become a client and receive exclusive service offerings.</p>
+            <button className="bg-black text-white rounded-md h-8 w-32 md:w-64 md:h-16 text-sm md:text-xl font-medium mt-4 mb-4">Request Service</button>
           </div>
           <div className="flex flex-col items-center">
             <Image
